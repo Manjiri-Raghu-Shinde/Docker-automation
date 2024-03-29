acbb99838e5a08dad6bcac5ac72ca851ad3d7af9
Manjiri-Raghu-Shinde <manjiri.r.shinde@gmail.com>
2024-03-29 16:50:41 +0530

    add files

---
+++ src/main/java/SeleniumAssignment/SimpleJavaApplication/App.java
@@ -0,0 +1,13 @@
+package SeleniumAssignment.SimpleJavaApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/SeleniumAssignment/SimpleJavaApplication/AppTest.java
@@ -0,0 +1,14 @@
+package SeleniumAssignment.SimpleJavaApplication;
+
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	@Test
+	public void sampleTest() {
+		System.out.println("this is a sample test");
+	}
+}
